db1729873afcf3be7418e5cd8ad6587ed235385d
Sudarshan Patil <105053040+Mr-Sudarshan-Patil@users.noreply.github.com>
2024-07-23 09:47:10 -0700

    Update Signup.js

--- src/landing_page/signup/Signup.js
+++ src/landing_page/signup/Signup.js
@@ -27,7 +27,7 @@ function Signup() {
       return handleError('Name, email, and password are required');
     }
     try {
-      const url = "http://localhost:8080/auth/signup";
+      const url = "https://zerodha-backend.vercel.app/auth/signup";
       const response = await fetch(url, {
         method: "POST",
         headers: {
